1ddda443636b92d9bf34313ee3eb93a34fba39dd
Csilla Bukki <csillabukki@Csillas-MacBook-Air.local>
2024-06-09 16:12:35 -0700

    step 3 useAxios added: Cards

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import axios from "axios";
+import { v1 as uuid } from "uuid";
+
 const useFlip = () => {
   const [state, setState] = useState(true);
   const flipCard = () => {
@@ -6,5 +9,30 @@ const useFlip = () => {
   };
   return [state, flipCard];
 };
+const useAxios = (url) => {
+  const [responses, setResponses] = useState([]);
+  const addResponse = async () => {
+    const res = await axios.get(url);
+    setResponses((responses) => [...responses, { ...res.data, id: uuid() }]);
+  };
+  console.log("responses", responses);
+  return [responses, addResponse];
+};
 
-export { useFlip };
+/*function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
+
+  const addResponseData = async (
+    formatter = (data) => data,
+    restOfUrl = ""
+  ) => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses((data) => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+}*/
+
+export { useFlip, useAxios };
